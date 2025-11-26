d8aedf6ab4ce22ebcb7f47ce49752e5239a56779
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-11-26 22:12:16 +0800

    learned some of the basics in typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import Arrays from "./components/Arrays.tsx";
+import Button from "./components/Button.tsx";
+import Index from "./components/index.tsx";
 const App = () => {
   return (
-    <div className="text-lg text-yellow-400 ">This is typescript practice</div>
+    <div className=" ">
+      <Arrays />
+      <Index />
+      <Button backgroundColor="blue" fontSize={10} />
+    </div>
   );
 };
 
---
+++ frontend/src/components/index.tsx
@@ -0,0 +1,14 @@
+let sales: number = 1234568;
+let is_published: boolean = true;
+let course: string = "Typescript";
+let level;
+level = true;
+
+function render(document: any) {
+  console.log(document);
+}
+const Index = () => {
+  return <div></div>;
+};
+
+export default Index;
